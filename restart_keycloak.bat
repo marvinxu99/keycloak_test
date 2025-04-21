d5bcf09cce905d17cd8d9ad1ceb1d20dd6d5215b
Marvin Xu <marvinxu99@hotmail.com>
2025-04-21 08:01:58 -0700

    ...

---
+++ restart_keycloak.bat
@@ -0,0 +1,5 @@
+REM docker run -d --name keycloak -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak start-dev
+
+REM docker stop keycloak
+
+docker restart keycloak
